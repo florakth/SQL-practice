e868bed828623a51c044c1ac9ac6ed410fa5b4b5
florakth <50193916+florakth@users.noreply.github.com>
2020-04-18 12:51:11 +0200

    Add files via upload

--- SQL Practice.py
+++ SQL Practice.py
@@ -42,3 +42,30 @@ duplicates.
 
 SELECT DISTINCT CITY FROM STATION WHERE
 CITY NOT REGEXP '^[aeiou]' OR CITY NOT REGEXP '[aeiou]$' ;
+
+6. Write a query to output the start and end dates of projects
+listed by the number of days it took to complete the project in ascending order.
+If there is more than one project that have the same number of completion days,
+then order by the start date of the project.
+
+SELECT Start_Date, MIN(End_Date) FROM 
+(SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects) ) AS s,
+(SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) AS e
+WHERE Start_Date < End_Date
+GROUP BY Start_Date
+ORDER BY DATEDIFF(MIN(End_Date), Start_Date), Start_Date;
+
+7. Write a query to output the names of those students whose best friends
+got offered a higher salary than them. Names must be ordered by the salary
+amount offered to the best friends. It is guaranteed that no two students
+got same salary offer.
+
+SELECT s.Name FROM
+( SELECT s.ID ID, s.Name Name, p.Salary Salary, f.Friend_ID FID from 
+ Students s 
+ JOIN Friends f ON f.ID = s.ID
+ JOIN Packages p ON p.ID = s.ID) S
+ JOIN Students s1 ON s1.ID = S.FID
+ JOIN Packages fp ON fp.ID = S.FID
+WHERE S.Salary < fp.Salary
+ORDER BY fp.Salary;
